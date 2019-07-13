ae05efac159d5164bbf4bb680bd985eaaafff9c9
“Fiona <fcohen@mica.edu>
2019-07-13 19:57:17 -0400

    adding form fills to letter footer

--- script.js
+++ script.js
@@ -19,14 +19,69 @@ $(document).ready(function($) {
         $('textarea').removeAttr("readonly");
     });
 
-    $('#parent-fname').change(function() {
+    $('#fname').change(function() {
       let inputValue = $(this).val();
       let letter = $('#letter');
 
       letter.val(letter.val() + `\nFirst Name: ${inputValue}`)
     })
 
+    $('#lname').change(function() {
+      let inputValue = $(this).val();
+      let letter = $('#letter');
+
+      letter.val(letter.val() + `\nLast Name: ${inputValue}`)
+    })
+
+    $('#email').change(function() {
+      let inputValue = $(this).val();
+      let letter = $('#letter');
+
+      letter.val(letter.val() + `\nEmail: ${inputValue}`)
+    })
+
+    $('#phone').change(function() {
+      let inputValue = $(this).val();
+      let letter = $('#letter');
+
+      letter.val(letter.val() + `\nPhone: ${inputValue}`)
+    })
    
+    $('#addr1').change(function() {
+      let inputValue = $(this).val();
+      let letter = $('#letter');
+
+      letter.val(letter.val() + `\nAddress: ${inputValue}`)
+    })
+
+    $('#addr2').change(function() {
+      let inputValue = $(this).val();
+      let letter = $('#letter');
+
+      letter.val(letter.val() + `\nAddress Line 2: ${inputValue}`)
+    })
+
+    $('#city').change(function() {
+      let inputValue = $(this).val();
+      let letter = $('#letter');
+
+      letter.val(letter.val() + `\nCity: ${inputValue}`)
+    })
+
+    $('#state').change(function() {
+      let inputValue = $(this).val();
+      let letter = $('#letter');
+
+      letter.val(letter.val() + `\nState: ${inputValue}`)
+    })
+
+    $('#zip').change(function() {
+      let inputValue = $(this).val();
+      let letter = $('#letter');
+
+      letter.val(letter.val() + `\nZip Code: ${inputValue}`)
+    })
+
 });
 
 //clear textarea
